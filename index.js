97d2feb2382b15cf101fb6fe981284baf8057623
HarendraD <harendradilshan57@gmail.com>
2022-05-14 10:13:48 +0530

    Start

---
+++ index.js
@@ -0,0 +1,9 @@
+const canvas=document.getElementById("game");
+const ctx=canvas.getContext("2d");
+
+const background= new Image();
+background.src="images/background.png";
+
+function game(){
+    
+}
